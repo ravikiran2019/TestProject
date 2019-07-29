e7b0d0aa64c75df0886a4d26b66ac6b7bd8f53ce
JK <JK@LAPTOP-FN8U3CV9>
2019-07-29 09:10:08 +0530

    First build

---
+++ JENKINS_DEMO/src/com/qa/jenkinspractice/Jenkins.java
@@ -0,0 +1,14 @@
+package com.qa.jenkinspractice;
+
+import org.testng.annotations.Test;
+
+public class Jenkins {
+	
+	
+	@Test
+	public void function()
+	{
+		System.out.println("Welcome to Jenkins");
+	}
+
+}
